be493b334efec5339adef99bcf73dfd628bdc2bf
Laroub03 <ayoublaroub@gmail.com>
2023-09-07 13:32:39 +0200

    Initial commit

---
+++ Web-Server/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace WebServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           // to create new one:
+            Server server = new Server();
+
+            var ipAddress = System.Net.IPAddress.Parse("127.0.0.1");
+            var port = 80;
+            var maxconnections = 10;
+            var contentpath = "C:\\Users\\Administrator\\source\\repos\\Web-Server\\Web-Server\\HelloWorld";
+
+            // to start it
+            server.start(ipAddress, port, maxconnections, contentpath);
+        }
+    }
+}
